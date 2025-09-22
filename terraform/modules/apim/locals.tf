2f42863b64e29347c88406990efda4ae1d541c67
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-09-22 19:22:19 -0300

    refactor: remove route preffix

--- terraform/modules/apim/locals.tf
+++ terraform/modules/apim/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  auth_api_backend = "https://${data.terraform_remote_state.infra.outputs.azfunc_private_dns_fqdn}/api"
-  auth_api_validate_endpoint = "https://${data.terraform_remote_state.infra.outputs.azfunc_private_dns_fqdn}/api/validate"
+  auth_api_backend = "https://${data.terraform_remote_state.infra.outputs.azfunc_private_dns_fqdn}"
+  auth_api_validate_endpoint = "https://${data.terraform_remote_state.infra.outputs.azfunc_private_dns_fqdn}/validate"
 }
