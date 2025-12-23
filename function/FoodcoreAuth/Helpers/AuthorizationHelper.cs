a01d7867b5c59f55a4e0bd3cd57e705fa3413958
Pedro <ferrarezzolabs@outlook.com>
2025-12-22 23:51:59 -0300

    fix: fix AuthorizationHelper regexp

--- function/FoodcoreAuth/Helpers/AuthorizationHelper.cs
+++ function/FoodcoreAuth/Helpers/AuthorizationHelper.cs
@@ -14,28 +14,28 @@ namespace Foodcore.Auth.Helpers
         public static readonly List<SecurityRules> Rules =
         [
             #region Public Endpoints
-            new SecurityRules { HttpMethod = "POST", Pattern = @"^/payments/webhook$", AllowedRoles = null },
+            new SecurityRules { HttpMethod = "POST", Pattern = @"^/payment/webhook$", AllowedRoles = null },
             #endregion
 
             #region Order Endpoints
-            new SecurityRules { HttpMethod = "POST", Pattern = @"^/orders$", AllowedRoles = [Role.CUSTOMER.ToString()] },
-            new SecurityRules { HttpMethod = "PATCH", Pattern = @"^/orders/.*$", AllowedRoles = [Role.ADMIN.ToString()] },
-            new SecurityRules { HttpMethod = "GET", Pattern = @"^/orders/[^/]+$", AllowedRoles = [Role.ADMIN.ToString()] },
-            new SecurityRules { HttpMethod = "GET", Pattern = @"^/orders/active$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "POST", Pattern = @"^/order$", AllowedRoles = [Role.CUSTOMER.ToString()] },
+            new SecurityRules { HttpMethod = "PATCH", Pattern = @"^/order/.*$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "GET", Pattern = @"^/order/[^/]+$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "GET", Pattern = @"^/order/active$", AllowedRoles = [Role.ADMIN.ToString()] },
             #endregion
 
             #region Catalog Endpoints
-            new SecurityRules { HttpMethod = "GET", Pattern = @"^/catalogs.*$", AllowedRoles = [Role.ADMIN.ToString(), Role.CUSTOMER.ToString()] },
-            new SecurityRules { HttpMethod = "POST", Pattern = @"^/catalogs.*$", AllowedRoles = [Role.ADMIN.ToString()] },
-            new SecurityRules { HttpMethod = "PATCH", Pattern = @"^/catalogs.*$", AllowedRoles = [Role.ADMIN.ToString()] },
-            new SecurityRules { HttpMethod = "PUT", Pattern = @"^/catalogs.*$", AllowedRoles = [Role.ADMIN.ToString()] },
-            new SecurityRules { HttpMethod = "DELETE", Pattern = @"^/catalogs.*$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "GET", Pattern = @"^/catalog.*$", AllowedRoles = [Role.ADMIN.ToString(), Role.CUSTOMER.ToString()] },
+            new SecurityRules { HttpMethod = "POST", Pattern = @"^/catalog.*$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "PATCH", Pattern = @"^/catalog.*$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "PUT", Pattern = @"^/catalog.*$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "DELETE", Pattern = @"^/catalog.*$", AllowedRoles = [Role.ADMIN.ToString()] },
             #endregion
 
             #region Payment Endpoints
-            new SecurityRules { HttpMethod = "GET", Pattern = @"^/payments/[^/]+/qrCode$", AllowedRoles = [Role.ADMIN.ToString(), Role.CUSTOMER.ToString()] },
-            new SecurityRules { HttpMethod = "GET", Pattern = @"^/payments/[^/]+/status$", AllowedRoles = [Role.ADMIN.ToString(), Role.CUSTOMER.ToString()] },
-            new SecurityRules { HttpMethod = "GET", Pattern = @"^/payments/merchant_orders/[^/]+$", AllowedRoles = [Role.ADMIN.ToString()] },
+            new SecurityRules { HttpMethod = "GET", Pattern = @"^/payment/[^/]+/qrCode$", AllowedRoles = [Role.ADMIN.ToString(), Role.CUSTOMER.ToString()] },
+            new SecurityRules { HttpMethod = "GET", Pattern = @"^/payment/[^/]+/status$", AllowedRoles = [Role.ADMIN.ToString(), Role.CUSTOMER.ToString()] },
+            new SecurityRules { HttpMethod = "GET", Pattern = @"^/payment/merchant_orders/[^/]+$", AllowedRoles = [Role.ADMIN.ToString()] },
             #endregion
         ];
     }
