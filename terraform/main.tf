eaa5eada8c1a93c9022445cac7e27149b0b6940d
Pedro <ferrarezzolabs@outlook.com>
2025-12-23 21:58:09 -0300

    fix: add subscription id variable in apim terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ module "apim" {
   foodcore-backend-infra-key        = var.foodcore-backend-infra-key
   foodcore-backend-resource-group   = var.foodcore-backend-resource-group
   foodcore-backend-storage-account  = var.foodcore-backend-storage-account
+  subscription_id                   = var.subscription_id
   apim_api_auth_name                = var.apim_api_auth_name
   apim_api_auth_version             = var.apim_api_auth_version
   auth_api_path                     = local.auth_api_path_without_slash
